3232a1aeec38d960f138978a053059e62e62a8e2
DESKTOP-NS7ESGA\Sam <omegaonion@gmail.com>
2019-05-16 19:16:19 +0100

    +img011smaller , tolerance 3

--- src/Test.java
+++ src/Test.java
@@ -1,16 +1,15 @@
-import java.io.IOException;
-import java.awt.Color;
-import java.awt.Point;
+import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
-import javax.imageio.ImageIO;
 
 public class Test {
     public Test() throws IOException {
 
 
-        BufferedImage image = ImageIO.read(new File("img011smaller.png"));
+        BufferedImage image = ImageIO.read(new File("img011.jpg"));
         boolean[][] points = new FloodFill().floodFill(image, new Point(1, 1), Color.WHITE, Color.RED);
         ImageIO.write(image, "png", new File("floodfill.png"));
 
